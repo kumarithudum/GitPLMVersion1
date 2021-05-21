cd101e814a566508d687e7550d7074808bbdcf98
Kumari <kumari.ch@gmail.com>
2021-05-21 11:59:43 +0530

    Commited the project

---
+++ src/com/git/plm/PLMTest.java
@@ -0,0 +1,8 @@
+package com.git.plm;
+
+public class PLMTest {
+	public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GitPLMVersion1 {
+}
